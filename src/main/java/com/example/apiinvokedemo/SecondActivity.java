4852866b8b312a617b1f4c24b74d0c8250522b20
thompson <lt19891121@live.cn>
2016-10-21 15:41:31 +0800

    星座运势完成

---
+++ src/main/java/com/example/apiinvokedemo/SecondActivity.java
@@ -0,0 +1,135 @@
+package com.example.apiinvokedemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+/**
+ * Created by thompson on 16-10-21.
+ */
+
+public class SecondActivity extends Activity {
+    private RelativeLayout contentLayout;
+    private ImageView constImage;
+    private TextView today;
+    private TextView week;
+    private LinearLayout loadLayout;
+//    private TextView mTextView;
+    private int[] res = new int[]{R.drawable.num_1,R.drawable.num_2,R.drawable.num_3,R.drawable.num_4,
+            R.drawable.num_5,R.drawable.num_6,R.drawable.num_7,R.drawable.num_8,
+            R.drawable.num_9,R.drawable.num_10,R.drawable.num_11,R.drawable.num_12};
+    private String[] name={"%e7%99%bd%e7%be%8a%e5%ba%a7","%e9%87%91%e7%89%9b%e5%ba%a7","%e5%8f%8c%e5%ad%90%e5%ba%a7","%e5%b7%a8%e8%9f%b9%e5%ba%a7",
+            "%e7%8b%ae%e5%ad%90%e5%ba%a7","%e5%a4%84%e5%a5%b3%e5%ba%a7","%e5%a4%a9%e7%a7%a4%e5%ba%a7","%e5%a4%a9%e8%9d%8e%e5%ba%a7",
+            "%e5%b0%84%e6%89%8b%e5%ba%a7","%e6%91%a9%e7%be%af%e5%ba%a7","%e6%b0%b4%e7%93%b6%e5%ba%a7","%e5%8f%8c%e9%b1%bc%e5%ba%a7"};
+    String httpUrl = "http://apis.baidu.com/bbtapi/constellation/constellation_query";
+    String httpArg = "?consName=";
+    String httpArg1 = "&type=";
+    private int position;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String[] as = (String[]) msg.obj;
+            constImage.setImageResource(res[position]);
+            today.setText(as[0]);
+            week.setText(as[1]);
+            contentLayout.setVisibility(VISIBLE);
+            loadLayout.setVisibility(GONE);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_second);
+        //初始化内容组件
+        contentLayout = (RelativeLayout) findViewById(R.id.container_content);
+        constImage = (ImageView) findViewById(R.id.const_image);
+        today = (TextView) findViewById(R.id.today);
+        week = (TextView) findViewById(R.id.week);
+        //初始化缓冲组件
+        loadLayout = (LinearLayout) findViewById(R.id.fresh_content);
+//        mTextView = (TextView) findViewById(R.id.text);
+//        String s=getJson("http://apis.baidu.com/bbtapi/constellation/constellation_query?consName=山羊座&type=today");
+//        mTextView.setText(s);
+        Intent intent =getIntent();
+        position = intent.getIntExtra(SecondFragment.EXTRA_WHAT,0);
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                String http1=httpUrl+httpArg+name[position]+httpArg1+"today";
+                String http2=httpUrl+httpArg+name[position]+httpArg1+"week";
+                try {
+                    JSONObject jsonObject_one = new JSONObject(getJson(http1));
+                    String datetime ="日期："+jsonObject_one.getString("datetime");
+                    String name ="星座："+jsonObject_one.getString("name");
+                    String QFriend="速配："+jsonObject_one.getString("QFriend");
+                    String health="健康指数："+jsonObject_one.getString("health");
+                    String love="爱情指数："+jsonObject_one.getString("love");
+                    String money="财运指数："+jsonObject_one.getString("money");
+                    String work="工作指数："+jsonObject_one.getString("work");
+                    String color="幸运色："+jsonObject_one.getString("color");
+                    String number="幸运号："+jsonObject_one.getString("number");
+                    String summary="总结："+jsonObject_one.getString("summary");
+                    JSONObject jsonObject_two = new JSONObject(getJson(http2));
+                    String heal=jsonObject_two.getString("health");
+                    String job=jsonObject_two.getString("job");
+                    String lover=jsonObject_two.getString("love");
+                    String mone=jsonObject_two.getString("money");
+                    String worker=jsonObject_two.getString("work");
+                    String[] sa = {datetime+"\n"+name+"\n"+QFriend+"\n"+health+"\n"+love+"\n"+money+"\n"+work+"\n"+color+"\n"+number+"\n"+summary+"\n",
+                            heal+"\n"+job+"\n"+mone+"\n"+lover+"\n"+worker+"\n"};
+                    Message msg = Message.obtain();
+                    msg.obj = sa;
+                    mHandler.sendMessage(msg);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private String getJson(String str){
+        StringBuilder sb = null;
+        try {
+            URL url = new URL(str);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("apikey", "b7ab2b71060688d2813b170d6f866e8b");
+            InputStream input = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input,"UTF-8"));
+            sb = new StringBuilder();
+            String line="";
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+}
--- src/main/java/com/example/apiinvokedemo/SecondFragment.java
+++ src/main/java/com/example/apiinvokedemo/SecondFragment.java
@@ -2,188 +2,59 @@ package com.example.apiinvokedemo;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by thompson on 16-10-18.
  */
 public class SecondFragment extends Fragment {
-    private static final String TAG = "FirstFragment";
-
     private ListView mListView;
-    private List<NewsPager> newsList;
-    private List<String> res;
-    //    private TextView mTextView;
-    private static String httpUrl = "http://apis.baidu.com/showapi_open_bus/channel_news/search_news";
-    private static String httpArg = "channelId=5572a10ab3cdc86cf39001eb"
-            + "&channelName=%E5%9B%BD%E5%86%85%E6%9C%80%E6%96%B0"
-            + "&title=%E4%B8%8A%E5%B8%82"
-            + "&page=1"
-            + "&needContent=1"
-            + "&needHtml=0";
-    private String getUrl=httpUrl+"?"+httpArg;
-    private MySQLiteOpenHelper helper;
-    private SQLiteDatabase readableDatabase;
-    private Cursor cursor;
-    private Context mContext;
-    private LinearLayout mLinearLayout;
+    private String[] array = {"白羊座:3月21日～4月20日",
+            "金牛座:4月21日～5月20日",
+            "双子座:5月21日～6月21日",
+            "巨蟹座:6月22日～7月22日",
+            "狮子座:7月23日～8月22日",
+            "处女座:8月23日～9月22日",
+            "天秤座:9月23日～10月23日",
+            "天蝎座:10月24日～11月22日",
+            "射手座:11月23日～12月21日",
+            "摩羯座:12月22日～1月19日",
+            "水瓶座:1月20日～2月18日",
+            "双鱼座:2月19日～3月20日"};
+    private Activity mActivity;
 
-
-    private Handler mHandler=new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            newsList = (List<NewsPager>) msg.obj;
-            mListView.setVisibility(View.VISIBLE);
-            mLinearLayout.setVisibility(View.GONE);
-            initListView();
-
-        }
-    };
-
-    private void initListView(){
-        for(int i=0;i<newsList.size();i++){
-            res.add(newsList.get(i).getTitle());
-        }
-        Log.d(TAG, "res="+res);
-        Log.d(TAG, "getActivity="+getActivity());
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext,android.R.layout.simple_list_item_1,res);
-
-
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getActivity(), ContentActivity.class);
-                intent.putExtra("title",newsList.get(position).getTitle());
-                intent.putExtra("content", newsList.get(position).getContent());
-                intent.putExtra("time", newsList.get(position).getTime());
-                startActivity(intent);
-            }
-        });
-    }
+    public static final String EXTRA_WHAT = "com.example.apiinvokedemo";
 
     @Override
-    public void onAttach(Activity context) {
-        super.onAttach(context);
-        mContext = context;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        helper = new MySQLiteOpenHelper(getActivity(),"News.db",null,1);
-        newsList = new ArrayList<NewsPager>();
-        res = new ArrayList<String>();
-
-        readableDatabase = helper.getWritableDatabase();
-        cursor = readableDatabase.rawQuery("select * from launtwo",null);
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = activity;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-        mLinearLayout = (LinearLayout) view.findViewById(R.id.loading);
-        mListView = (ListView) view.findViewById(R.id.newstitle);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        mListView = (ListView) view.findViewById(R.id.list_item);
 
-        if(cursor.moveToFirst()) {
-            mListView.setVisibility(View.VISIBLE);
-            mLinearLayout.setVisibility(View.GONE);
-            do{
-                String title = cursor.getString(cursor.getColumnIndex("title"));
-                String content = cursor.getString(cursor.getColumnIndex("content"));
-                String time = cursor.getString(cursor.getColumnIndex("time"));
-                NewsPager np = new NewsPager(content, title, time);
-                newsList.add(np);
-            }while(cursor.moveToNext());
-            initListView();
-        }else{
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    BufferedReader reader = null;
-                    StringBuilder sb = null;
-                    try {
-                        URL url = new URL(getUrl);
-                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                        httpURLConnection.setRequestMethod("GET");
-                        httpURLConnection.setRequestProperty("apikey", "b7ab2b71060688d2813b170d6f866e8b");
-                        InputStream inputStream = httpURLConnection.getInputStream();
-                        reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                        sb = new StringBuilder();
-                        String line = "";
-                        while ((line = reader.readLine()) != null) {
-                            sb.append(line);
-//                            sb.append("\r\n");
-                        }
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        if (reader != null)
-                            try {
-                                reader.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                    }
-                    List<NewsPager> pagerList = new ArrayList<NewsPager>();
-                    try {
-                        JSONObject jsonObject_level_one=new JSONObject(sb.toString());
-                        JSONObject jsonObject_level_two = jsonObject_level_one.getJSONObject("showapi_res_body");
-                        JSONObject jsonObject_level_three = jsonObject_level_two.getJSONObject("pagebean");
-                        JSONArray jsonArray = jsonObject_level_three.getJSONArray("contentlist");
-                        for(int i=0;i<jsonArray.length();i++)
-                        {
-                            JSONObject contentObject=jsonArray.getJSONObject(i);
-                            String title = contentObject.getString("title");
-                            String content = contentObject.getString("content");
-                            String time = contentObject.getString("pubDate");
-                            readableDatabase.execSQL("insert into launtwo(title,time,content) values(?,?,?)", new String[]{title, time, content});
-                            NewsPager np = new NewsPager(content, title, time);
-                            pagerList.add(np);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    Message msg = Message.obtain();
-                    msg.obj = pagerList;
-                    mHandler.sendMessage(msg);
-                }
-            }).start();
-        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(mActivity, android.R.layout.simple_list_item_1, array);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(mActivity, SecondActivity.class);
+                intent.putExtra(EXTRA_WHAT, position);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
